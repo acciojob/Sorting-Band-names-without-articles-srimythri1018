585c20c0ae6228c1a66b1b5319badf92b23f6045
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-04-26 07:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+// the array of band names
+let bandNames = ['The Beatles', 'Led Zeppelin', 'The Rolling Stones', 'Pink Floyd', 'AC/DC'];
+
+// a function to remove articles from a band name
+function removeArticle(name) {
+  let articles = ['a', 'an', 'the'];
+  let words = name.split(' ');
+  if (articles.includes(words[0].toLowerCase())) {
+    words.shift();
+  }
+  return words.join(' ');
+}
+
+// sort the band names in lexicographic order
+bandNames.sort(function(a, b) {
+  return removeArticle(a) > removeArticle(b) ? 1 : -1;
+});
+
+// add the sorted band names to the ul element
+let bandList = document.getElementById('band');
+for (let name of bandNames) {
+  let li = document.createElement('li');
+  li.textContent = name;
+  bandList.appendChild(li);
+}
 
