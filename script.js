6ead0dc7317dbc1a067d28a1173a6c8d308df8a1
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-05-06 23:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//your code here
-// the array of band names
-let bandNames = ['The Beatles', 'Led Zeppelin', 'The Rolling Stones', 'Pink Floyd', 'AC/DC'];
-
-// a function to remove articles from a band name
-function removeArticle(name) {
-  let articles = ['a', 'an', 'the'];
-  let words = name.split(' ');
-  if (articles.includes(words[0].toLowerCase())) {
-    words.shift();
-  }
-  return words.join(' ');
-}
-
-// sort the band names in lexicographic order
-bandNames.sort(function(a, b) {
-  return removeArticle(a) > removeArticle(b) ? 1 : -1;
+let arr=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+arr.map((element)=>{
+	let words=element.split(" ");
+	let updatedstring=words.reduce((prev, current, index)=> {
+		if(current!= "The" && current!= "an" && current!= "a"){
+			return prev+" "+ current;
+		}
+		return prev;
+	},"")
+	return updatedstring;
 });
-
-// add the sorted band names to the ul element
-let bandList = document.getElementById('band');
-for (let name of bandNames) {
-  let li = document.createElement('li');
-  li.textContent = name;
-  bandList.appendChild(li);
-}
-
+let mp ={};
+updtedList.forEach((element)=>{
+	mp[element]=arr[i];
+});
+updatedList.sort();
+let ans=updatedList.map((element)=>{
+	return mp[element];
+})
+console.log(ans);
